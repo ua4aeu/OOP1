27ae88a43687adfbca2448394a3cbc812efaa2eb
ua4aeu <ra4a@yandex.ru>
2020-10-20 09:41:19 +0300

    Add files via upload

---
+++ 1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StudentPKS student1 = new StudentPKS
+            {
+                FirstName = "Илья",
+                LastName = "Василенко",
+                Age = 18,
+                GrupNamber = "ПКС1-18"
+            }; // конструктор student1
+            student1.Print(student1); // вызываем метод Print с параметрами student1
+
+            StudentPKS student2 = new StudentPKS
+            {
+                FirstName = "Василий",
+                LastName = "Петров",
+                Age = 19,
+                GrupNamber = "ПКС1-17"
+            };
+            student2.Print(student2); // вызываем метод Print с параметрами student2
+
+            Console.ReadKey();
+        }
+    }
+}
+// https://www.youtube.com/watch?v=PUeR3srCtyM
+
---
+++ 1/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1
+{
+    
+
+    class StudentPKS
+    {
+        public string FirstName; // поле класса
+        public string LastName; // поле класса
+        public int Age; // поле класса
+        public string GrupNamber; // поле класса
+        public void Print(StudentPKS student) // метод класса
+        {
+            Console.WriteLine($"{GrupNamber} {student.FirstName} {student.LastName} {student.Age} ");
+        }
+    }
+}
